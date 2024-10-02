8f6c58636091f26a4d875885a532e7147ce0e202
vin <vin@vin.vin>
2024-10-02 14:15:02 +0530

    Taking value from input

---
+++ script.js
@@ -0,0 +1,28 @@
+let myLeads= [];
+
+
+const inputBtn = document.getElementById("input-btn");
+const  inputEl= document.getElementById("input-el");
+const ulEl= document.getElementById("ul-el")
+
+
+inputBtn.addEventListener("click",function(){
+  myLeads.push(inputEl.value);
+  renderLeads();
+  clearField();
+})
+
+function clearField(){
+  inputEl.value= '';
+}
+
+function renderLeads(){
+  let listItems= "";
+  for (let i = 0; i<myLeads.length;i++){
+    listItems+= `<li> <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]}</a></li> `;
+
+    
+  }
+  ulEl.innerHTML= listItems;
+}
+
