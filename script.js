0a53cba6525cc96e5f8a2d08b6e65ab15d709fca
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-10-03 12:20:12 +0530

    Added chrome API

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-let myLeads= [];
-
+let myLeads = [];
 
 const inputBtn = document.getElementById("input-btn");
-const  inputEl= document.getElementById("input-el");
-const ulEl= document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
 
-inputBtn.addEventListener("click",function(){
-  myLeads.push(inputEl.value);
-  renderLeads();
-  clearField();
-})
-
-function clearField(){
-  inputEl.value= '';
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-function renderLeads(){
-  let listItems= "";
-  for (let i = 0; i<myLeads.length;i++){
-    listItems+= `<li> <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]}</a></li> `;
 
-    
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li> <a href='${leads[i]}' target='_blank'> ${leads[i]}</a></li> `;
   }
-  ulEl.innerHTML= listItems;
+  ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  clearField();
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+function clearField() {
+  inputEl.value = "";
+}
+
+// CRHOME API USED
+
+// TODO
+// -> Add delete button for individual leads
+// -> History option
+// -> Refractor in React
+// -> Make sure the extension doesn't disappear upon switching tabs
