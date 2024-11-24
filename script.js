2dabc66ea9fa9827040b27e6b0bd582dd26a39e3
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-11-24 11:38:25 +0530

    Added copy button functionality and rest of the icons

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
-const exportBtn = document.getElementById("export-btn"); // Add this line
+const exportBtn = document.getElementById("export-btn");
+const copyAllBtn = document.getElementById("copy-all-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
@@ -13,15 +14,33 @@ if (leadsFromLocalStorage) {
   render(myLeads);
 }
 
-// Add the export functionality
+copyAllBtn.addEventListener("click", function () {
+  const leads = JSON.parse(localStorage.getItem("myLeads"));
+  if (leads && leads.length > 0) {
+    const allLeads = leads.join("\n");
+    navigator.clipboard
+      .writeText(allLeads)
+      .then(() => {
+        copyAllBtn.innerHTML = `<img src="./icons/check.svg" class="copy-icon">`;
+        setTimeout(() => {
+          copyAllBtn.innerHTML = `<img src="./icons/copy.svg" class="copy-icon">`;
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+        alert("Failed to copy links!");
+      });
+  } else {
+    alert("No links to copy!");
+  }
+});
 exportBtn.addEventListener("click", function () {
   const leads = JSON.parse(localStorage.getItem("myLeads"));
   if (leads && leads.length > 0) {
-    let csvContent = "data:text/csv;charset=utf-8,URL\n"; // Header
+    let csvContent = "data:text/csv;charset=utf-8,URL\n";
     leads.forEach(function (url) {
       csvContent += url + "\n";
     });
-
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
@@ -34,7 +53,6 @@ exportBtn.addEventListener("click", function () {
   }
 });
 
-// Rest of your existing code remains the same
 tabBtn.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     myLeads.push(tabs[0].url);
@@ -46,9 +64,28 @@ tabBtn.addEventListener("click", function () {
 function render(leads) {
   let listItems = "";
   for (let i = 0; i < leads.length; i++) {
-    listItems += `<li> <a href='${leads[i]}' target='_blank'> ${leads[i]}</a></li> `;
+    listItems += `
+            <li>
+                <span class="delete-item" data-index="${i}">x</span>
+                <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
+            </li>
+        `;
   }
   ulEl.innerHTML = listItems;
+
+  const deleteButtons = document.querySelectorAll(".delete-item");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const index = parseInt(e.target.getAttribute("data-index"));
+      deleteIndividualLead(index);
+    });
+  });
+}
+
+function deleteIndividualLead(index) {
+  myLeads.splice(index, 1);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 }
 
 deleteBtn.addEventListener("dblclick", function () {
